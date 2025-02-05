51bb4ffb9db3c2cb821ecf0cd53e5df222602bd9
Noel Burkhardt <101584947+PythonTilk@users.noreply.github.com>
2025-02-05 23:24:59 +0100

    Removed sth uneccesery

--- valentines-day-app/src/app.js
+++ valentines-day-app/src/app.js
@@ -3,7 +3,91 @@ document.addEventListener('DOMContentLoaded', () => {
     const noButton = document.getElementById('noButton');
     const createLinkForm = document.getElementById('createLinkForm');
     const linkContainer = document.getElementById('linkContainer');
+    const valentineMessage = document.getElementById('valentineMessage');
     let noClickCount = 0;
+
+    const beggingPhrases = [
+        "Please reconsider!",
+        "Are you sure?",
+        "Think about it!",
+        "Don't say no!",
+        "Give it a chance!",
+        "Please!",
+        "Just one more time!",
+        "I beg you!",
+        "Pretty please!",
+        "Don't break my heart!",
+        "Please don't say no!",
+        "I'm begging you!",
+        "Please think again!",
+        "Don't do this!",
+        "Please don't!",
+        "Just say yes!",
+        "Please, please!",
+        "I'm on my knees!",
+        "Don't make me cry!",
+        "Please be kind!",
+        "I'm pleading!",
+        "Please be my Valentine!",
+        "Don't say no again!",
+        "Please, I'm asking nicely!",
+        "Just this once!",
+        "Please don't reject me!",
+        "I'm asking you!",
+        "Please, it's Valentine's Day!",
+        "Don't make me sad!",
+        "Please, I'm desperate!",
+        "Just say yes, please!",
+        "Please, it's important!",
+        "Don't say no, please!",
+        "Please, I'm serious!",
+        "Just one yes!",
+        "Please, I'm trying!",
+        "Don't make me beg!",
+        "Please, it's special!",
+        "Just this time!",
+        "Please, I'm hopeful!",
+        "Don't say no, think!",
+        "Please, I'm sincere!",
+        "Just say yes, please!",
+        "Please, it's a request!",
+        "Don't say no, please!",
+        "Please, I'm asking!",
+        "Just one yes, please!",
+        "Please, it's love!",
+        "Don't say no, please!",
+        "Please, I'm here!",
+        "Just say yes, please!",
+        "Please, it's a wish!",
+        "Don't say no, please!",
+        "Please, I'm waiting!",
+        "Just one yes, please!",
+        "Please, it's a dream!",
+        "Don't say no, please!",
+        "Please, I'm hoping!",
+        "Just say yes, please!",
+        "Please, it's a plea!",
+        "Don't say no, please!",
+        "Please, I'm here!",
+        "Just one yes, please!",
+        "Please, it's a hope!",
+        "Don't say no, please!",
+        "Please, I'm asking!",
+        "Just say yes, please!",
+        "Please, it's a wish!",
+        "Don't say no, please!",
+        "Please, I'm waiting!",
+        "Just one yes, please!",
+        "Please, it's a dream!",
+        "Don't say no, please!",
+        "Please, I'm hoping!",
+        "Just say yes, please!",
+        "Please, it's a plea!",
+        "Don't say no, please!",
+        "Please, I'm here!",
+        "Just one yes, please!",
+        "Please, it's a hope!"
+    ];
 
     createLinkForm.addEventListener('submit', (event) => {
         event.preventDefault();
@@ -30,7 +114,7 @@ document.addEventListener('DOMContentLoaded', () => {
         } while (
             (randomX < yesButton.offsetLeft + yesButton.offsetWidth && randomX + noButton.offsetWidth > yesButton.offsetLeft) ||
             (randomY < yesButton.offsetTop + yesButton.offsetHeight && randomY + noButton.offsetHeight > yesButton.offsetTop) ||
-            (randomY < document.getElementById('valentineMessage').offsetTop + document.getElementById('valentineMessage').offsetHeight)
+            (randomY < valentineMessage.offsetTop + valentineMessage.offsetHeight)
         );
 
         noButton.style.position = 'absolute';
@@ -39,5 +123,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const currentScale = parseFloat(window.getComputedStyle(yesButton).transform.split(',')[0].slice(7)) || 1;
         yesButton.style.transform = `scale(${currentScale + 0.1})`;
+        valentineMessage.style.marginTop = `${-currentScale * 20}px`;
+
+        noButton.textContent = beggingPhrases[noClickCount % beggingPhrases.length];
     });
 });
